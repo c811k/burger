7a5de22ce276029ce0c53057e62164cf830a8c1a
c811k <bhk811@gmail.com>
2019-01-15 01:23:48 -0800

    style

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", "true");
-INSERT INTO burgers (burger_name, devoured) VALUES ("cheese burger", "false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("turkey burger", "true");
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheese burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("turkey burger");
